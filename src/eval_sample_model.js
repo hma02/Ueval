abb57d19196300a7580c2192cba84067bdac9cee
He <hma02@users.noreply.github.com>
2017-12-01 18:26:42 -0500

    update TODOs

--- src/eval_sample_model.js
+++ src/eval_sample_model.js
@@ -1,6 +1,8 @@
 // TODOs:
 // 1. made critic Initialization same by setting random seed, but can't make input provider Math.random() with a seed
+// 2. improve UI description
 // 3. upload images to evaluate
+// 4. Get better initial gen from GAN demo
 
 class Net { // gen or disc or critic
     constructor(name, archType, modelConfigs) {
